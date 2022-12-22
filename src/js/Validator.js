ec3f1a0bd18a48f2dce1dce0ece7e991b098c6ab
Alexander Vasnev <a-s-vasnev88@yandex.ru>
2022-12-22 15:01:11 +0300

    change Validator

--- src/js/Validator.js
+++ src/js/Validator.js
@@ -1,19 +1,21 @@
 export default class Validator {
-  constructor(name) {
-    if (this.validateUsername(name)) {
-      this.name = name;
-    } else {
-      throw new Error('Имя пользователя может содежржать только латинские буквы, символы тире, подчёркивания и цифры не более 3 подряд');
+  static validateUsername(name) {
+    const result = [true];
+    if (!/^[a-z0-9-_]+$/gi.test(name)) {
+      result[0] = false;
+      result.push('Допустимы только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9)');
     }
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  validateUsername(name) {
-    let result = false;
-    const valid = /^[a-z][a-z0-9-_]+[a-z]$/gi;
-    const numberValidation = /[0-9]{4}/;
-    if (valid.test(name) && !numberValidation.test(name)) {
-      result = true;
+    if (!/^[a-z]/i.test(name)) {
+      result[0] = false;
+      result.push('Имя не должно начинаться цифрами, символами подчёркивания или тире');
+    }
+    if (!/[a-z]$/i.test(name)) {
+      result[0] = false;
+      result.push('Имя не должно заканчиваться цифрами, символами подчёркивания или тире');
+    }
+    if (/[0-9]{4}/.test(name)) {
+      result[0] = false;
+      result.push('Имя не должно содержать подряд более трёх цифр');
     }
     return result;
   }
--- src/js/__test__/Validator.test.js
+++ src/js/__test__/Validator.test.js
@@ -1,13 +1,36 @@
 import Validator from '../Validator';
 
 test('Validator test valid name', () => {
-  const obj = new Validator('user-one_123_pro');
-  expect(obj.name).toBe('user-one_123_pro');
+  const result = Validator.validateUsername('user-one_123_pro');
+  expect(result).toEqual([true]);
+});
+
+test('Validator test general validation', () => {
+  const result = Validator.validateUsername('user-one_*123_pro');
+  expect(result).toEqual([false, 'Допустимы только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9)']);
+});
+
+test('Validator test beginning of name', () => {
+  const result = Validator.validateUsername('1user-one_123_pro');
+  expect(result).toEqual([false, 'Имя не должно начинаться цифрами, символами подчёркивания или тире']);
+});
+
+test('Validator test end of name', () => {
+  const result = Validator.validateUsername('user-one_123_pro6');
+  expect(result).toEqual([false, 'Имя не должно заканчиваться цифрами, символами подчёркивания или тире']);
+});
+
+test('Validator test numberValidation', () => {
+  const result = Validator.validateUsername('user-one_1234_pro');
+  expect(result).toEqual([false, 'Имя не должно содержать подряд более трёх цифр']);
 });
 
 test('Validator test invalid name', () => {
-  function ValidatorName() {
-    return new Validator('user-one_1234_pro');
-  }
-  expect(ValidatorName).toThrow('Имя пользователя может содежржать только латинские буквы, символы тире, подчёркивания и цифры не более 3 подряд');
+  const result = Validator.validateUsername('user-one_1234_pro!');
+  expect(result).toEqual([
+    false,
+    'Допустимы только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9)',
+    'Имя не должно заканчиваться цифрами, символами подчёркивания или тире',
+    'Имя не должно содержать подряд более трёх цифр',
+  ]);
 });
